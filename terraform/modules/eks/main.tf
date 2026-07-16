988928ddb239b3f93acb549b6c5aa750313644f0
oreoluwaonietan <oreoluwaonietan@gmail.com>
2026-07-16 09:44:59 +0000

    Fix node group instance type to t3.small

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -60,7 +60,7 @@ resource "aws_eks_node_group" "main" {
   node_group_name = "starttech-node-group"
   node_role_arn   = aws_iam_role.node_group.arn
   subnet_ids      = var.private_subnet_ids
-  instance_types  = ["t3.medium"]
+  instance_types  = ["t3.small"]
 
   scaling_config {
     desired_size = 2
