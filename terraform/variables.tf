1227f77be68aa499cbf956b0eb5e96f7b84353a7
oreoluwaonietan <oreoluwaonietan@gmail.com>
2026-07-16 14:12:11 +0000

    Point CloudFront ALB origin to real ALB from AWS Load Balancer Controller

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -13,7 +13,7 @@ variable "vpc_cidr" {
 variable "alb_dns_name" {
   description = "ALB DNS name created by EKS ingress controller"
   type        = string
-  default     = "placeholder.us-east-1.elb.amazonaws.com"
+  default     = "k8s-default-backendi-598781ecdc-983593658.us-east-1.elb.amazonaws.com"
 }
 # retrigger Thu Jul 16 11:20:28 UTC 2026
 # retrigger Thu Jul 16 11:38:06 UTC 2026
